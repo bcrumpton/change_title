ad1ca206d2ba91c8d379adcd63fdd1c5995a02aa
Brannon Crumpton <bcrumpton72@gmail.com>
2017-05-31 15:29:18 -0500

    Developed plugin to change titles to foo bar

---
+++ change_title_foobar.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Change Title to "Foo Bar"
+ * Plugin URI:
+ * Description: This plugin adds changes all titles to "Foo Bar".
+ * Version: 1.0.0
+ * Author: Brannon Crumpton
+ * Author URI: http://bcrumpton.com
+ */
+
+add_filter('the_title', 'change_title_foobar');
+function change_title_foobar($title) {
+	return 'Foo Bar';
+}
